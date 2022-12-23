5ea88e1a8a91ab64be4d44b34eba70aa7f21e855
OmnipotentOwl <1769881+OmnipotentOwl@users.noreply.github.com>
2022-12-22 22:51:54 -0500

    fix: split provider and terraform version control apart.

---
+++ versions.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.24"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = ">= 2.5"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0"
+    }
+    azapi = {
+      source  = "azure/azapi"
+      version = ">=0.6"
+    }
+    github = {
+      source  = "integrations/github"
+      version = ">= 4.5.2"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.2"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.10.0"
+    }
+    azurecaf = {
+      source  = "aztfmod/azurecaf"
+      version = ">= 1.2.23"
+    }
+  }
+  required_version = ">= 1.3.0"
+}
